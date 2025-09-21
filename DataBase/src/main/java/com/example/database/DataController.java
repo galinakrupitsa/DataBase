6b5e2bc6ddd40b42cbc11f566ed66a4d9c719b0e
galina <galina.krupitsa@gmail.com>
2025-09-21 23:39:14 +0500

    l

--- DataBase/src/main/java/com/example/database/DataController.java
+++ DataBase/src/main/java/com/example/database/DataController.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.*;
 import javax.xml.crypto.Data;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 public class DataController {
     final DataService dataService;
 
     public DataController(DataService dataService) {
         this.dataService = dataService;
     }
-    @PostMapping("/user/{userId}")
+    @PostMapping("/user/{userId}/create")
     public String saveData(@PathVariable String userId, @RequestBody DataClass dataClass) {
-        return dataClass.getLastName() + dataClass.getLastName();
+        return dataClass.getName() +" " + dataClass.getLastName();
     }
 }
